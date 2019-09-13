ac6261a2b04024d11a8d9dee293d8d8dbbbf0359
szczygiel9 <szczygielski.daniel38@gmail.com>
2019-09-13 20:40:59 +0200

    Updates

---
+++ precache-manifest.332edabc48df7f31081f01c3b6ca1c34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad60db4b09bcac190751445de36963f5",
+    "url": "/Protfolio/index.html"
+  },
+  {
+    "revision": "56d54a87af3258254f08",
+    "url": "/Protfolio/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "aa97a88911a7185e0e5d",
+    "url": "/Protfolio/static/js/2.16a58cdf.chunk.js"
+  },
+  {
+    "revision": "56d54a87af3258254f08",
+    "url": "/Protfolio/static/js/main.e86931d1.chunk.js"
+  },
+  {
+    "revision": "eed2befa50023f50cfe9",
+    "url": "/Protfolio/static/js/runtime~main.11e7d3d2.js"
+  }
+]);
