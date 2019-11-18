af2403eb6da718627d8874b6beed3c7c7242626a
szczygiel9 <szczygielski.daniel38@gmail.com>
2019-11-18 11:45:43 +0100

    Switched headers to paragraphs

--- src/components/ExperienceSection.js
+++ src/components/ExperienceSection.js
@@ -3,12 +3,12 @@ import React from 'react';
 const ExperienceSection = ({title, content}) => {
     return (
         <div className='section'>
-            <h1 className='section-title'>{title}</h1>
+            <p className='section-title'>{title}</p>
             {content.map((experienceItem) => {
                 return (
                     <div>
-                        <h2 className='experience-position'>{experienceItem.position}</h2>
-                        <h3 className='experience-date'>{experienceItem.dateFrom} - {experienceItem.dateTo}</h3>
+                        <p className='experience-position'>{experienceItem.position}</p>
+                        <p className='experience-date'>{experienceItem.dateFrom} - {experienceItem.dateTo}</p>
                         <ul className='experience-list'>
                             {experienceItem.descriptionPoints.map((point) => {
                                 return <li>{point}</li>
