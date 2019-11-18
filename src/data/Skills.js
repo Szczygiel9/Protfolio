b51d17c71347cfa17ae83eb82798ee1922dd8c5e
szczygiel9 <szczygielski.daniel38@gmail.com>
2019-11-18 11:44:54 +0100

    Skills and summary data

---
+++ src/data/Skills.js
@@ -0,0 +1,12 @@
+const Skills = [
+    "Dobra znajomość języka Java oraz framework'a Spring",
+    "Sprawne posługiwanie się systemem kontroli wersji GIT",
+    "Praktyczna znajomość projektowania oraz implementacji serwisów typu REST",
+    "Praca z narzędziami: IntelliJ, Jenkins, Jira",
+    "Znajomość zagadnień mapowania obiektowo relacyjnego, w tym freamework'a Hibernate",
+    "Tworzenie testów jednostkowy przy użyciu biblioteki JUnit oraz Mockito",
+    "Znajomość składni SQL",
+    "Świadome wykorzystwyanie wzorców projektowych, dobrych praktyk oraz refactoringu",
+    "W trakcie nauki: Docker, AWS, JavaScript, React"];
+
+export default Skills;
