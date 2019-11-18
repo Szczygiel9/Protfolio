c05294baf445b3bdaa1b3265e100ca67f2a64258
szczygiel9 <szczygielski.daniel38@gmail.com>
2019-11-18 11:46:24 +0100

    Injected summary object

--- src/components/Description.js
+++ src/components/Description.js
@@ -1,12 +1,11 @@
 import React from 'react';
+import Summary from "../data/Summary";
 
 const Description = () => {
     return (
         <div className='section'>
-            <h1 className='section-title'>Informacje ogólne</h1>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In interdum vel sem non rutrum. Quisque bibendum tortor tempus porta scelerisque. Phasellus elementum nunc eget auctor lobortis. Donec ipsum nisl, convallis tristique rutrum ut, auctor quis risus. Vestibulum auctor dignissim erat at suscipit. Donec a suscipit lorem. Nullam eu justo sit amet ligula suscipit semper eu ut sapien.
-                Nulla dignissim vestibulum rhoncus. Aliquam sagittis ac ligula vitae mollis. Duis porta, justo ut congue maximus, orci enim vestibulum dolor, eu ultricies orci urna eu turpis. Mauris consectetur hendrerit nisi, at molestie quam pulvinar a. Aenean lobortis lorem mattis libero egestas, ut blandit est faucibus. Sed et tortor nec nibh imperdiet aliquam id vitae ante. Suspendisse vel nisi ornare, auctor ex ut, bibendum nisl. Pellentesque id elit vehicula, laoreet eros vitae, tempus metus. Duis eget dapibus nisi, et lobortis augue. Nunc auctor, nibh accumsan tincidunt condimentum, ipsum turpis hendrerit tellus, eu ultricies eros ligula non tellus. Etiam ac sapien consectetur, venenatis est efficitur, vulputate mauris.
+            <p className='summary'>
+                {Summary}
             </p>
         </div>
     );
