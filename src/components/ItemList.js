19ab5e82f8734d013f315995e9a2d99281d103c8
szczygiel9 <szczygielski.daniel38@gmail.com>
2019-11-18 14:36:19 +0100

    Simple sample project description, background gradient fix,

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,18 +1,68 @@
 import React from 'react';
-import Item from "./Item";
 
 const ItemList = ({projects}) => {
     return (
         <div className='item-list'>
-            {
-                projects.map(
-                    function (project) {
-                        return (
-                            <Item title={project.title} content={project.content}/>
-                        )
-                    }
-                )
-            }
+            <div className='item'>
+                <h1>Simple Sample</h1>
+
+                <p>Aplikacja pomagająca wypełniać bazę danych SQL testowymi danymi. Po podaniu struktury tabeli w
+                    postaci nazwy tabeli, nazw kolumn, typów kolun oraz potrzebnej liczby wierszy do wygenerowania
+                    użytkownik otrzymuje zapytanie INSERT, które wystarczy zaaplikować do swojej bazy danych.</p>
+
+                <p>Projekt został stworzony przy użyciu języka Java. Komunikacja została zapewniona poprzez REST api.
+                    Głównym endpoint'em dla użytkownika jest
+                    '/processTable', który pryjmuje w ciele apytania strukturę tabeli oraz zwraca porządane
+                    zapytanie. W celu utrwalenia przykładowych danych została wykorzystana baza danych NoSQL
+                    MongoDB.</p>
+
+                <p>Biblioteki wykorzystane w tym projekcie to:
+                    <ul>
+                        <li>Spring wraz z modułami: Core, Rest, Data oraz Spring Boot</li>
+                        <li>Lombok</li>
+                        <li>Apache Commons</li>
+                        <li>Swagger</li>
+                        <li>JUnit</li>
+                        <li>Jackson</li>
+                    </ul>
+                </p>
+
+                <p>Start serwera może się odbyć z dwoma głównymi profilami:
+                    <ul>
+                        <li>dev - dostarcza wszystkich niezbędych zależności do poprawnego generowania zapytań SQL</li>
+                        <li>data-init - ładuje przykładowe dane (np. imiona, miasta) do bazy, z którą komunikuje się
+                            aplikacja serwerowa. Dane do załadowania są wcześniej przygotowane w postaci plików
+                            tekstowy. Utrwalenie tych danych zostało zaimplementowane przy użyciu Spring’owych
+                            auto-repozytorów, które
+                            dostarczają podstawowe operacje CRUD.
+
+                        </li>
+                    </ul>
+                </p>
+
+                <p>
+                    Na podstawie zbudowanej paczki jar został stworzony obraz Docker. W celu sprawniejszego
+                    budowania obrazu w trakcie rozwijania aplikacji został użyty Dockerfile Maven plugin. Do poprawnego
+                    aplikacji działania niezbędna jest baza MongoDB, która została dostarczona również poprzez
+                    środowisko Docker. Kontener aplikacji i bazy danych zostały skomponowane przy użyciu docker-compose,
+                    a następnie wdrożone na platformę Amazon Web Services. Publiczny adres wystawionego api to:
+                    <a className='link'
+                       href="http://ec2-3-134-94-181.us-east-2.compute.amazonaws.com"> http://ec2-3-134-94-181.us-east-2.compute.amazonaws.com </a>
+                    Przykładowe request’y testujące można znależć <a className='link'
+                                                                     href='https://github.com/Szczygiel9/SimpleSample/blob/master/request-test/TableProcessingTest.http'>tutaj</a>
+
+                </p>
+            </div>
+
+            <div className='item'>
+                <h1>Portfolio</h1>
+                <p>
+                    To jest ten projekt ;)
+                    //TODO
+                    //TODO
+                </p>
+            </div>
+
         </div>
     );
 };
--- src/data/SampleProjects.js
+++ src/data/SampleProjects.js
@@ -1,63 +1,12 @@
-const sampleProject1 = {
-    title: "Sample Title1",
-    content: "Sample content1"
+const simpleSample = {
+    title: "Simple sample",
+    content: "Simple-sample Springowe moduły i biblioteki: Core, REST, Lombok, Apache Commons... Utrwalenie danych zostało zaimplementowane przy użyciu Spring’owych auto-repozytorów, które dostarczają podstawowe operacje CRUD. Przykładowe dane do generowania wyjściowych zapytań SQL są ładowane z plików tekstowych przy uruchomieniu aplikacji.Ładowanie danych z pliku tekstowegoprofile z data init, mock i devNa podstawie zbudowanej paczki jar został stworzony obraz Docker. W celu sprawniejszego budowania obrazu w trakcie rozwijania aplikacji został użyty Dockerfile Maven plugin. Do poprawnego aplikacji działania niezbędna jest baza MongoDB, która została dostarczona również poprzez środowisko Docker. Kontener aplikacji i bazy danych zostały skomponowane przy użyciu docker-compose, a następnie wdrożone na platformę Amazon Web Services. Publiczny adres wystawionego api to: http://ec2-3-134-94-181.us-east-2.compute.amazonaws.com. Przykładowe request’y testujące można znależć <tutaj"
 };
 
-const sampleProject2 = {
-    title: "Sample Title2",
-    content: "Sample content2"
-};
-const sampleProject3 = {
-    title: "Sample Title2",
-    content: "Sample content2"
-};
-const sampleProject4 = {
-    title: "Sample Title2",
-    content: "Sample content2"
-};
-const sampleProject5 = {
-    title: "Sample Title2",
+const portfolio = {
+    title: "Portfolio",
     content: "Sample content2"
 };
 
-const projects = [sampleProject1,
-    sampleProject2,
-    sampleProject3,
-    sampleProject4,
-    sampleProject5,
-    sampleProject3,
-    sampleProject4,
-    sampleProject5,
-    sampleProject5,
-    sampleProject3,
-    sampleProject4,
-    sampleProject5,
-    sampleProject4,
-    sampleProject5,
-    sampleProject3,
-    sampleProject4,
-    sampleProject5,
-    sampleProject5,
-    sampleProject3,
-    sampleProject4,
-    sampleProject3,
-    sampleProject4,
-    sampleProject5,
-    sampleProject3,
-    sampleProject4,
-    sampleProject5,
-    sampleProject5,
-    sampleProject3,
-    sampleProject4,
-    sampleProject5,
-    sampleProject4,
-    sampleProject5,
-    sampleProject3,
-    sampleProject4,
-    sampleProject5,
-    sampleProject5,
-    sampleProject3,
-    sampleProject4,
-    sampleProject5
-];
+const projects = [simpleSample, portfolio];
 export default projects
