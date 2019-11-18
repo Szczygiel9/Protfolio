13f5fa367879d21045f650a0b1c9e68806bd00b5
szczygiel9 <szczygielski.daniel38@gmail.com>
2019-11-18 11:47:30 +0100

    Sidebar refactoring

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,24 +4,29 @@ const Sidebar = () => {
     return (
         <div className='sidebar'>
             <div className='image'></div>
-            <table className='table'>
-                <tr>
-                    <td>Imię i nazwisko:</td>
-                    <td>Daniel Szczygielski</td>
-                </tr>
-                <tr>
-                    <td>Email:</td>
-                    <td>szczygielski.daniel38@gmail.com</td>
-                </tr>
-                <tr>
-                    <td>LinkedIn:</td>
-                    <td><a href="https://www.linkedin.com/in/daniel-szczygielski-50b8a4154/">Profil</a></td>
-                </tr>
-                <tr>
-                    <td>GitHub:</td>
-                    <td><a href="https://github.com/Szczygiel9">szczygiel9</a></td>
-                </tr>
-            </table>
+            <div className='sidebar-content'>
+                <div className='under-lined'>Dane osobowe</div>
+                <div className='sidebar-section'>
+                    <div>Daniel Szczygielski</div>
+                    <div>szczygielski.daniel38@gmail.com</div>
+                </div>
+
+                <div className='under-lined'>Media</div>
+                <div className='sidebar-section'>
+                    <div>
+                        LinkedIn: <a className='link'
+                                    href="https://www.linkedin.com/in/daniel-szczygielski-50b8a4154/">Profil</a>
+                    </div>
+                    <div>
+                        GitHub: <a className='link' href="https://github.com/Szczygiel9">szczygiel9</a>
+                    </div>
+                </div>
+
+                <div className='under-lined'>Język</div>
+                <div className='sidebar-section'>
+                    Angielski w stopniu umożliwiającym płynną komunikacje z członkami zespołu
+                </div>
+            </div>
         </div>
     );
 };
