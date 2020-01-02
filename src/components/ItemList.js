38a3b0eb897997a6567f67052c4bd6a69370aca0
szczygiel9 <szczygielski.daniel38@gmail.com>
2020-01-02 21:53:16 +0100

    Projects content and AWS image

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,19 +1,28 @@
 import React from 'react';
+import aws from '../resources/aws.jpg'
 
 const ItemList = ({projects}) => {
     return (
         <div className='item-list'>
+
             <div className='item'>
-                <h1>Simple Sample</h1>
+                <h1>SimpleSample</h1>
+
+                <p>
+                    Github: <a className='link' href='https://github.com/Szczygiel9/SimpleSample'>link</a>
+                </p>
 
                 <p>Aplikacja pomagająca wypełniać bazę danych SQL testowymi danymi. Po podaniu struktury tabeli w
-                    postaci nazwy tabeli, nazw kolumn, typów kolun oraz potrzebnej liczby wierszy do wygenerowania
-                    użytkownik otrzymuje zapytanie INSERT, które wystarczy zaaplikować do swojej bazy danych.</p>
+                    postaci nazwy tabeli, nazw kolumn, typów kolumn, opcjonalnych typów instynktownych
+                    oraz potrzebnej liczby wierszy do wygenerowania użytkownik otrzymuje zapytanie INSERT,
+                    które wystarczy zaaplikować do swojej bazy danych.</p>
 
                 <p>Projekt został stworzony przy użyciu języka Java. Komunikacja została zapewniona poprzez REST api.
-                    Głównym endpoint'em dla użytkownika jest
-                    '/processTable', który pryjmuje w ciele apytania strukturę tabeli oraz zwraca porządane
-                    zapytanie. W celu utrwalenia przykładowych danych została wykorzystana baza danych NoSQL
+                    Głównym endpoint'em dla użytkownika jest '/processTable', który przyjmuje w ciele zapytania strukturę
+                    tabeli oraz zwraca pożądany kod SQL. Poza typem kolumny możliwe jest podanie typu instynktowego.
+                    Jest jedna z wartości predefiniowanych typów, dla których zostanie wczytana wartość ze zbioru przykładowych
+                    danych (np. Anna dla typu 'name', Microsoft dla typu 'company).
+                    W celu utrwalenia przykładowych danych została wykorzystana baza danych NoSQL
                     MongoDB.</p>
 
                 <p>Biblioteki wykorzystane w tym projekcie to:
@@ -27,39 +36,53 @@ const ItemList = ({projects}) => {
                     </ul>
                 </p>
 
-                <p>Start serwera może się odbyć z dwoma głównymi profilami:
-                    <ul>
-                        <li>dev - dostarcza wszystkich niezbędych zależności do poprawnego generowania zapytań SQL</li>
-                        <li>data-init - ładuje przykładowe dane (np. imiona, miasta) do bazy, z którą komunikuje się
-                            aplikacja serwerowa. Dane do załadowania są wcześniej przygotowane w postaci plików
-                            tekstowy. Utrwalenie tych danych zostało zaimplementowane przy użyciu Spring’owych
-                            auto-repozytorów, które
-                            dostarczają podstawowe operacje CRUD.
-
-                        </li>
-                    </ul>
+                <p>
+                    Start serwera może się odbyć z profilem data-init - ładuje przykładowe dane (np. imiona, miasta)
+                    do bazy, z którą komunikuje się aplikacja serwerowa. Dane do załadowania są wcześniej przygotowane
+                    w postaci plików tekstowych. Utrwalenie tych danych zostało zaimplementowane przy użyciu Spring’owych
+                    auto-repozytorów, które dostarczają podstawowe operacje CRUD.
                 </p>
 
                 <p>
-                    Na podstawie zbudowanej paczki jar został stworzony obraz Docker. W celu sprawniejszego
-                    budowania obrazu w trakcie rozwijania aplikacji został użyty Dockerfile Maven plugin. Do poprawnego
-                    aplikacji działania niezbędna jest baza MongoDB, która została dostarczona również poprzez
-                    środowisko Docker. Kontener aplikacji i bazy danych zostały skomponowane przy użyciu docker-compose,
-                    a następnie wdrożone na platformę Amazon Web Services. Publiczny adres wystawionego api to:
-                    <a className='link'
-                       href="http://ec2-3-134-94-181.us-east-2.compute.amazonaws.com"> http://ec2-3-134-94-181.us-east-2.compute.amazonaws.com </a>
-                    Przykładowe request’y testujące można znależć <a className='link'
-                                                                     href='https://github.com/Szczygiel9/SimpleSample/blob/master/request-test/TableProcessingTest.http'>tutaj</a>
+                    Gotowa aplikacja została wdrożona na platformę AWS. W celu zbudowania aplikacji oraz jej deploymentu,
+                    został użyty serwis AWS CodePipeline wraz z narzędziami CodeBuild oraz CodeDeploy.
+                    Każdy wypchnięty commit do repozytorium GitHub wyzwala testy a następnie budowę paczki.
+                    Definicja pipeline'a została zawarta w pliku buildspec.yml. Sama aplikacja
+                    została wdrożona na serwis ElasticBeanstalk. Istancja aplikacji SimpleSample potrzebuje do działania
+                    bazy danych MongoDB. Jej instancja została zainstalowana na wirtualnej maszynie w serwisie EC2,
+                    przy użyciu środowiska Docker oraz oficjalnego obrazu MongoDB.
+                    Całość tworzy działąjącą strukturę CICD. Architektura została przedstawiona poniżej.
+                    <br/>
+                    <br/>
+                    <img src={aws} className='aws-image' alt="aws img"/>
+                    <br/>
+                    <br/>
 
+                    Publiczny adres wystawionego API to: <a className='link' href="http://simple-sample.us-east-2.elasticbeanstalk.com">link</a>.
+                    Testowanie jest również możliwe dzięki narzędziu Swagger, link: <a className='link' href="http://simple-sample.us-east-2.elasticbeanstalk.com/swagger-ui.html">swagger</a>.
                 </p>
             </div>
 
             <div className='item'>
                 <h1>Portfolio</h1>
                 <p>
-                    To jest ten projekt ;)
-                    //TODO
-                    //TODO
+                    Github: <a className='link' href='https://github.com/Szczygiel9/Portfolio'>link</a>
+                </p>
+                <p>
+                    Projekt tworzony w celu nauki JavaScript'u, CSS, React oraz HTML. Jest to statyczna strona
+                    internetowa będąca odpowiednikiem CV.
+                    <br/>
+                    Strona została zaprojektowana z dbałością o czytelność zarówno na urządzeniach o szerokim
+                    jak i wąskim ekranie. Do stworzenia nawigacji w górnej belce zostały użyte komponenty z pakietu
+                    'react-router-dom'. Sama aplikacja została wdrożona na serwis GitHub Pages.
+                </p>
+                <p>
+                    Do zrobienia:
+                    <ul>
+                        <li>obsługa przycisku zmiany języka</li>
+                        <li>uporządkowanie styli CSS</li>
+                        <li>poprawa responsywności oraz stylowaniana na urządzeniach mobilnych</li>
+                    </ul>
                 </p>
             </div>
 
