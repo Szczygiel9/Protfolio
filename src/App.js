316d053573a3248b2bb650fbc4b0ba30199e3411
szczygiel9 <szczygielski.daniel38@gmail.com>
2020-01-02 21:51:00 +0100

    Routing fix

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import React from 'react';
 import './App.css';
 import ItemList from "./components/ItemList";
 import Header from "./components/Header";
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {HashRouter as Router, Route} from 'react-router-dom';
 import About from "./components/About";
 import projects from "./data/SampleProjects";
 
 function App() {
     return (
-        <Router>
+        <Router basename={"/Portfolio/"}>
             <div className="App">
                 <Header/>
                 <div className='main-container'>
-                    <Route exact path={process.env.PUBLIC_URL + "/"} component={About}/>
-                    <Route path={process.env.PUBLIC_URL + "/projects"}
+                    <Route exact path={"/"} component={About}/>
+                    <Route path={"/projects"}
                            component={() => <ItemList projects={projects}/>}/>
                 </div>
             </div>
