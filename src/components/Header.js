d5eeed41e71215601e1761f1404ea892e92c5e01
szczygiel9 <szczygielski.daniel38@gmail.com>
2020-01-02 21:52:31 +0100

    Style classes rename

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import {NavLink} from "react-router-dom";
-import State from "../data/State";
 
 const Header = () => {
     return (
         <div className='nav'>
-            <NavLink exact activeClassName='ActiveLink' to="/">O mnie</NavLink>
-            <NavLink activeClassName='ActiveLink' to="/projects">Repozytoria warte wspomnienia</NavLink>
-            <button onClick={State.flipState} className='language-switcher'>PL | EN</button>
+            <NavLink exact activeClassName='active-link' to="/">O mnie</NavLink>
+            <NavLink activeClassName='active-link' to="/projects">Projekty warte wspomnienia</NavLink>
+            <button className='language-switcher'>PL | EN</button>
         </div>
     );
 };
